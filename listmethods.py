dc54249ecd824468bf6ba70984578642b4603593
Jesus <45245619+autismokramer@users.noreply.github.com>
2018-12-09 01:55:17 -0800

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,11 @@
+girls = ['Diana', 'Dyana' , 'Isabella' , 'Jade' , 'Trini']
+
+print(girls.index('Jade')) # using the index method to locate where jade is on the girls index list
+
+girls.append('Chelsea') # this adds Chelsea to the end of the girls list
+
+print(girls)
+
+girls.insert(2 , 'Tiffany') # this adds tiffany to the girls list inserting it as the third index in girls
+
+print(girls)
